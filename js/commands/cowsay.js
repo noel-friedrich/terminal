8a43ce16ae45384665ac80d372f6de1782c6879e
Noel Friedrich <noel.friedrich@outlook.de>
2023-01-22 22:24:18 +0100

    cow now able to moo multiline

--- js/commands/cowsay.js
+++ js/commands/cowsay.js
@@ -7,13 +7,54 @@ const COW_SAY = `
 `
 
 terminal.addCommand("cowsay", function(args) {
-    let message = args.message
+    const message = args.message
+    const bubbleWidth = Math.min(40, message.length)
+
+    function splitIntoLines(text, width=bubbleWidth) {
+        let lines = []
+        let line = String()
+        for (let word of text.split(" ")) {
+            if (line.length + word.length > width) {
+                lines.push(line)
+                line = String()
+            }
+            line += word + " "
+        }
+        lines.push(line)
+        return lines
+    }
+
+    let lines = splitIntoLines(args.message).map(l => l.trim())
+
     let output = String()
-    output += " " + stringMul("-", message.length + 2) + "\n"
-    output += "< " + message + " >\n"
-    output += " " + stringMul("-", message.length + 2)
+
+    output += " " + stringMul("-", bubbleWidth + 2) + "\n"
+    let i = 0
+    for (let line of lines) {
+        let leftChar = "|"
+        let rightChar = "|"
+        i++
+
+        if (lines.length >= 3) {
+            if (i == 1) {
+                leftChar = "/"
+                rightChar = "\\"
+            } else if (i == lines.length) {
+                leftChar = "\\"
+                rightChar = "/"
+            }
+        } else if (lines.length == 1) {
+            leftChar = "<"
+            rightChar = ">"
+        }
+
+        output += `${leftChar} ${stringPadMiddle(line, bubbleWidth)} ${rightChar}\n`
+    }
+    output += " " + stringMul("-", bubbleWidth + 2) + "\n"
+    output = output.slice(0, -1)
     for (let line of COW_SAY.split("\n")) {
-        output += stringMul(" ", Math.min(8, message.length + 4)) + line + "\n"
+        let amountSpaces = Math.max(4, Math.min(message.length, bubbleWidth - 20) + 4)
+        output += " ".repeat(amountSpaces) + line + "\n"
     }
     terminal.printLine(output.slice(0, -3))
 }, {
--- js/commands/cowthink.js
+++ js/commands/cowthink.js
@@ -1,4 +1,4 @@
-const COW_THINK = ` 
+const COW_SAY = ` 
 o   ^__^
  o  (oo)\\_______
     (__)\       )\\/\\
@@ -6,19 +6,58 @@ o   ^__^
         ||     ||
 `
 
-
 terminal.addCommand("cowthink", function(args) {
-    let message = args.message
+    const message = args.thought
+    const bubbleWidth = Math.min(40, message.length)
+
+    function splitIntoLines(text, width=bubbleWidth) {
+        let lines = []
+        let line = String()
+        for (let word of text.split(" ")) {
+            if (line.length + word.length > width) {
+                lines.push(line)
+                line = String()
+            }
+            line += word + " "
+        }
+        lines.push(line)
+        return lines
+    }
+
+    let lines = splitIntoLines(message).map(l => l.trim())
+
     let output = String()
-    output += " " + stringMul("-", message.length + 2) + "\n"
-    output += "( " + message + " )\n"
-    output += " " + stringMul("-", message.length + 2)
-    for (let line of COW_THINK.split("\n")) {
-        output += stringMul(" ", Math.min(8, message.length + 4)) + line + "\n"
+
+    output += " " + stringMul("-", bubbleWidth + 2) + "\n"
+    let i = 0
+    for (let line of lines) {
+        let leftChar = "("
+        let rightChar = ")"
+        i++
+
+        if (lines.length >= 3) {
+            if (i == 1) {
+                leftChar = "/"
+                rightChar = "\\"
+            } else if (i == lines.length) {
+                leftChar = "\\"
+                rightChar = "/"
+            }
+        } else if (lines.length == 1) {
+            leftChar = "("
+            rightChar = ")"
+        }
+
+        output += `${leftChar} ${stringPadMiddle(line, bubbleWidth)} ${rightChar}\n`
+    }
+    output += " " + stringMul("-", bubbleWidth + 2) + "\n"
+    output = output.slice(0, -1)
+    for (let line of COW_SAY.split("\n")) {
+        let amountSpaces = Math.max(4, Math.min(message.length, bubbleWidth - 20) + 4)
+        output += " ".repeat(amountSpaces) + line + "\n"
     }
     terminal.printLine(output.slice(0, -3))
 }, {
-    description: "let the cow think something",
-    args: ["*message"]
+    description: "let the cow say something",
+    args: ["*thought"]
 })
-
