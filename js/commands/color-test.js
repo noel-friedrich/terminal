04e43348283d9402de4f168b462d3930e0a7e329
Noel Friedrich <noel.friedrich@outlook.de>
2023-01-22 15:05:27 +0100

    removed console.log in color-test and new x ratio

--- js/commands/color-test.js
+++ js/commands/color-test.js
@@ -1,5 +1,5 @@
 terminal.addCommand("color-test", function() {
-    let size = {x: 68, y: 31}
+    let size = {x: 60, y: 31}
     for (let i = 0; i < size.y; i++) {
         for (let j = 0; j < size.x; j++) {
             let x = (j / size.x - 0.5) * 2
@@ -10,7 +10,6 @@ terminal.addCommand("color-test", function() {
                 let angle = Math.atan2(y, x) / Math.PI * 180
                 let hue = Math.round(angle)
                 let lightness = Math.round(90 - (x*x + y*y) * 90)
-                console.log(hue, lightness)
                 terminal.print("#", Color.hsl(hue / 360, 1, lightness / 100))
             }
         }
