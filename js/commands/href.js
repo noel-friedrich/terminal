070904edf9d28704d7641b39e7f773c6776ba20c
Noel Friedrich <noel.friedrich@outlook.de>
2023-02-03 21:15:41 +0100

    new base for href command

--- js/commands/href.js
+++ js/commands/href.js
@@ -1,8 +1,11 @@
 terminal.addCommand("href", function(args) {
+    if (!args.url.includes(".")) args.url = `noel-friedrich.de/${args.url}`
     if (!args.url.startsWith("http")) args.url = "https://" + args.url
     terminal.window.open(args.url, "_blank").focus()
 }, {
     description: "open a link in another tab",
-    args: ["url"]
+    args: {
+        "url": "url to open"
+    }
 })
 
