84f9c2f06c138da9f88ab199852df4836a6ce6c1
Noel Friedrich <noel.friedrich@outlook.de>
2023-02-18 18:42:40 +0100

    fixed echo indent bug

--- js/commands/echo.js
+++ js/commands/echo.js
@@ -1,5 +1,5 @@
 terminal.addCommand("echo", function(rawArgs) {
-    terminal.printLine(rawArgs)
+    terminal.printLine(rawArgs.slice(1))
 }, {
     description: "print a line of text",
     rawArgMode: true,
