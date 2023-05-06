302f517f8601ca5ae51ef155ea72d3fa47cda8ee
Noel Friedrich <noel.friedrich@outlook.de>
2023-05-06 21:32:43 +0200

    added history command limit

--- js/commands/history.js
+++ js/commands/history.js
@@ -1,13 +1,19 @@
 terminal.addCommand("history", function(args) {
+    let sliceLimit = args["show-full"] ? Infinity : 50
     let output = ""
     for (let i = Math.max(0, terminal.prevCommands.length - args.l); i < terminal.prevCommands.length; i++) {
-        output += `${i + 1}: ${terminal.prevCommands[i]}\n`
+        if (terminal.prevCommands[i].length >= sliceLimit) {
+            output += `${i + 1}: ${terminal.prevCommands[i].slice(0, sliceLimit)} [...]\n`
+        } else {
+            output += `${i + 1}: ${terminal.prevCommands[i]}\n`
+        }
     }
     terminal.printLine(output.slice(0, -1))
 }, {
     description: "print the command history",
     args: {
-        "?l=limit:n:1~100000": "the maximum number of commands to print"
+        "?l=limit:n:1~100000": "the maximum number of commands to print",
+        "?show-full:b": "show the full command instead of the shortened version"
     },
     standardVals: {
         l: 1000
