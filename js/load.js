26482a66186c435601efc34e10c7ed6f3cd67026
Noel Friedrich <noel.friedrich@outlook.de>
2023-08-20 13:02:12 +0200

    updated error views

--- js/load.js
+++ js/load.js
@@ -17,3 +17,19 @@ async function initalLoadScripts() {
 }
 
 initalLoadScripts()
+
+document.querySelector("#loading-terminal-container").style.display = "block"
+
+setTimeout(() => {
+    if (window.terminal && window.terminal.hasInitted) {
+        return
+    }
+
+    let loadingContainer = document.querySelector("#loading-terminal-container")
+
+    if (loadingContainer) {
+        loadingContainer.style.display = "none"
+        document.querySelector("#loading-terminal-failed-container").style.display = "block"
+    }
+
+}, 5000)
