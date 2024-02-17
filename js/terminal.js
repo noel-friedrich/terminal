dd87781b880c0397c2af3323a48bfdae1dde3740
Noel Friedrich <noel.friedrich@outlook.de>
2024-02-17 21:00:46 +0100

    fixed typo

--- js/terminal.js
+++ js/terminal.js
@@ -148,7 +148,7 @@ class TerminalFile {
         if (obj.type === "directory") {
             // compatability with previous versions of filesystem saving
             // that may still exist in some peoples localstorage!
-            if (typeof a == "object" && a !== null) {
+            if (typeof obj.content == "object" && obj.content !== null) {
                 obj.content = Object.entries(obj.content).map(([fname, fobj]) => {
                     fobj.name = fname
                     return fobj
